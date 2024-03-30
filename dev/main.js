e3bf2e7522d064df64c360232ec523759dd4e484
Yolanare <y09a3115@gmail.com>
2024-03-30 20:32:31 +0100

    scroll zoom-in accurate to top of element

--- dev/main.js
+++ dev/main.js
@@ -555,8 +555,8 @@ const layoutZoomInElements = doc.querySelectorAll("[onscroll-zoom_in]");
 if (layoutZoomInElements) {
     layoutZoomInElements.forEach((el) => {
         el.setAttribute("data-scroll-offset", "0");
-        el.setAttribute("onscroll-zoom_in--fraction", (el.getAttribute("onscroll-zoom_in--fraction")) ? el.getAttribute("onscroll-zoom_in--fraction") : 2.5);
-        el.setAttribute("onscroll-zoom_in--strength", (el.getAttribute("onscroll-zoom_in--strength")) ? el.getAttribute("onscroll-zoom_in--strength") : 0.925);
+        el.setAttribute("onscroll-zoom_in--fraction", (el.getAttribute("onscroll-zoom_in--fraction")) ? el.getAttribute("onscroll-zoom_in--fraction") : 2);
+        el.setAttribute("onscroll-zoom_in--strength", (el.getAttribute("onscroll-zoom_in--strength")) ? el.getAttribute("onscroll-zoom_in--strength") : 0.92);
         el.style.setProperty("--zoom-factor", parseFloat(el.getAttribute("onscroll-zoom_in--strength")));
     })
 }
@@ -566,10 +566,17 @@ scrollDoc.on('scroll', (args) => {
               progressFactor = mapRangeClamp(
                     (doc.clientHeight - elRect.top),
                     0,
+                    doc.clientHeight / parseFloat(el.getAttribute("onscroll-zoom_in--fraction")),
+                    parseFloat(el.getAttribute("onscroll-zoom_in--strength")),
+                    1
+              );
+              /* progressFactor = mapRangeClamp(
+                    (doc.clientHeight - elRect.top),
+                    0,
                     (doc.clientHeight + elRect.height) / parseFloat(el.getAttribute("onscroll-zoom_in--fraction")),
                     parseFloat(el.getAttribute("onscroll-zoom_in--strength")),
                     1
-                );
+              ); */
 
         el.style.setProperty("--zoom-factor", progressFactor);
     })
