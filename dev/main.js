f6e67cabd1a63fcbdb7a9c6dbe32ef3a0000bf90
Yolanare <y09a3115@gmail.com>
2024-04-04 02:18:16 +0200

    scroll intensity

--- dev/main.js
+++ dev/main.js
@@ -289,7 +289,7 @@ const ScrollMain = new LocomotiveScroll({
     lenisOptions: {
         smoothWheel: true,
         smoothTouch: false,
-        wheelMultiplier: 0.85,
+        wheelMultiplier: 0.8,
         duration: 1.25,
         easing: (x) => Math.min(1, 1.001 - Math.pow(5, -6.1 * x)), // https://www.desmos.com/calculator/brs54l4xou
         orientation: 'vertical',
