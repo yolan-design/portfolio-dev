77163aeba68978fc31e2153028437221e4030029
Yolanare <y09a3115@gmail.com>
2024-04-08 02:09:55 +0200

    p

---
+++ dev/import/projects.js
@@ -0,0 +1,25 @@
+const projects = {
+    "chacun_ses_gouts" : {
+        /*
+        data
+            titre : Chacun ses Goûts
+            sous-titre : Vidéo promotionnelle fictive
+            date : 2022
+            type : Projet Scolaire
+            category : ["motion", "3D"]
+
+        layout
+            color
+                accent
+                fill
+                bg
+
+
+        */
+    }
+}
+
+
+export {
+    projects
+};
